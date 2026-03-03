6b90d45fcb3f069f5e2db7d950cea97266db5690
Augustin Nguyen <144770199+yamiap@users.noreply.github.com>
2026-03-03 10:42:54 -0800

    Format outputs file

--- terraform/modules/web-server/outputs.tf
+++ terraform/modules/web-server/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_ip" {
-  value = aws_instance.web.public_ip
+  value       = aws_instance.web.public_ip
   description = "The instance IP address"
 }
 
 output "instance_dns" {
-  value = aws_instance.web.public_dns
+  value       = aws_instance.web.public_dns
   description = "The instance DNS name"
 }
 
 output "instance_id" {
-  value = aws_instance.web.id
+  value       = aws_instance.web.id
   description = "The instance ID"
 }
